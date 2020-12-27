6b66fcbd5aecd3aee74a557c0a804759328b1ced
Mustafa kunwa <m.kunwa52@gmail.com>
2020-12-27 15:18:25 +0530

    used context API instead of redux

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from 'react';
 
+import { ProductContext } from '../context/products-context';
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  const productList = useContext(ProductContext).products;
+
   return (
     <ul className="products-list">
       {productList.map(prod => (
